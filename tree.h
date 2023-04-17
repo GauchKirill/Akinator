cb0bec7988f11c17c672447376347c7fd4e31958
Gauch Kirill <kgauch@mail.ru>
2023-04-17 11:29:19 +0300

    увеличил размер констант

--- tree.h
+++ tree.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const size_t MAX_LENGHT_DATA      = 50;
-const size_t MAX_LENGHT_NAME_FILE = 20;
+const size_t MAX_LENGHT_DATA      = 100;
+const size_t MAX_LENGHT_NAME_FILE = 100;
 const size_t MAX_LENGHT_ANSWER    = 4;
 
 typedef char* elem_t;
